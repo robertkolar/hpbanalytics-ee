f73fb9f69cb5ec6fc59e1b9f117eb67a3c902986
Robert Kolar <robert.kolar@highpowerbear.com>
2016-05-31 10:39:18 +0000

    Rename message queue names in iblogger

--- han-iblogger-logic/src/main/java/com/highpowerbear/hpbanalytics/iblogger/message/MqSender.java
+++ han-iblogger-logic/src/main/java/com/highpowerbear/hpbanalytics/iblogger/message/MqSender.java
@@ -18,24 +18,24 @@ public class MqSender {
     private static final Logger l = Logger.getLogger(IbLoggerDefinitions.LOGGER);
 
     @Inject private JMSContext jmsContext;
-    @Resource(lookup = "java:/jms/queue/iblogger_c2")
-    private Queue ibloggerToC2Q;
-    @Resource(lookup = "java:/jms/queue/iblogger_report")
-    private Queue ibloggerToReportQ;
+    @Resource(lookup = "java:/jms/queue/IbLoggerToC2Q")
+    private Queue ibLoggerToC2Q;
+    @Resource(lookup = "java:/jms/queue/IbLoggerToReportQ")
+    private Queue ibLoggerToReportQ;
 
     public void sendToC2(String requestXml) {
-        l.info("BEGIN send message to MQ=iblogger_c2, xml=" + requestXml);
+        l.info("BEGIN send message to MQ=IbLoggerToC2Q, xml=" + requestXml);
         JMSProducer producer = jmsContext.createProducer();
         TextMessage message = jmsContext.createTextMessage(requestXml);
-        producer.send(ibloggerToC2Q, message);
-        l.info("END send message to MQ=iblogger_c2");
+        producer.send(ibLoggerToC2Q, message);
+        l.info("END send message to MQ=IbLoggerToC2Q");
     }
 
     public void sendToReport(String executionXml) {
-        l.info("BEGIN send message to MQ=iblogger_report, xml=" + executionXml);
+        l.info("BEGIN send message to MQ=IbLoggerToReportQ, xml=" + executionXml);
         JMSProducer producer = jmsContext.createProducer();
         TextMessage message = jmsContext.createTextMessage(executionXml);
-        producer.send(ibloggerToReportQ, message);
-        l.info("END send message to MQ=iblogger_report");
+        producer.send(ibLoggerToReportQ, message);
+        l.info("END send message to MQ=IbLoggerToReportQ");
     }
 }
